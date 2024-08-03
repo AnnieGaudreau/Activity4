1b430317188b146ae215457b9270adf86e7867b2
AnnieGaudreau <annalee.gaudreau@gmail.com>
2024-08-03 14:19:10 -0400

    Update activity4_1.py

--- activity4_1.py
+++ activity4_1.py
@@ -9,7 +9,7 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 st.title('Diabetes Data')
 df = pd.read_csv('https://storage.googleapis.com/scsu-data-science/diabetes_nan.csv')
